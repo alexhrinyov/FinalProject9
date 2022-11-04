d9ffdc7040f4c7401e3d19cbd3b4a3a242fedac4
alexhrinyov <hrinyou29@gmail.com>
2022-11-04 19:34:27 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static Exception[] exceptions;
+        static void Main(string[] args)
+        {
+            Exception1 Exception1 = new Exception1();
+            ArgumentException Exception2 = new ArgumentException();
+            ArgumentOutOfRangeException Exception3 = new ArgumentOutOfRangeException();
+            ArgumentNullException Exception4 = new ArgumentNullException();
+            DirectoryNotFoundException Exception5 = new DirectoryNotFoundException();
+            exceptions = new Exception[5] {Exception1, Exception2, Exception3, Exception4, Exception5 };
+
+            foreach (var item in exceptions)
+            {        
+                try
+                {
+                    throw item;
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }       
+        }
+
+    }
+    class Exception1 : Exception
+    {
+        static string message = "Новое исключение";
+        public Exception1() : base(message)
+        {
+
+        }
+    }
+    
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static List<string> names = new List<string>() { "Elizabet", "Johann", "Victoria", "Sebastian", "Steven" };
+
+        static void Main(string[] args)
+        {
+            // descending - нисходящий, вверху списка будут имена с последними буквами(по возрастанию)
+            // ascending - восходящий, вверху списка будут имена с первыми буквами(по убыванию)
+            UserChoise userChoise = new UserChoise();
+            //регистрация события и перехватывание
+            userChoise.ChoiseDoneEvent += KeyPressedHandler;
+            //вызов метода в классе-подписчике
+            userChoise.StartRead();
+
+        }
+        // перехватчик события - метод с сигнатурой делегата
+        public static void KeyPressedHandler(ConsoleKeyInfo PressedKey)
+        {
+            // сортировка и вывод
+            switch (PressedKey.Key)
+            {
+                case ConsoleKey.D1:
+                    {
+                        // сортировка по возрастанию
+                        // " из Имя в Именах Упорядочить Имя по Возрастанию, Добавить Имя в collection"
+                        var descending = from name in names
+                                         orderby name descending
+                                         select name;
+                        foreach (var item in descending)
+                        {
+                            WriteLine(item);
+                        }
+                        WriteLine("Нажмите любую кнопку.");
+                        ReadKey();
+                    }
+                    break;
+                case ConsoleKey.D2:
+                    {
+                        // сортировка по убыванию
+                        var ascending = from name in names
+                                        orderby name ascending
+                                        select name;
+                        WriteLine();
+                        foreach (var item in ascending)
+                        {
+                            WriteLine(item);
+                        }
+                        WriteLine("Нажмите любую кнопку.");
+                        ReadKey();
+                    }
+                    break;
+            }
+        }
+
+    }
+    // класс-подписчик
+    class UserChoise
+    {
+        public delegate void ChoiseDone(ConsoleKeyInfo PressedKey);
+        public event ChoiseDone ChoiseDoneEvent;
+        static MyException myException = new MyException();
+        public static ConsoleKeyInfo PressedKey;
+        public void StartRead()
+        { 
+            // бесконечный цикл, выбираем тип сортировки
+            while (true)
+            {
+                try
+                {
+                    Clear();
+                    WriteLine("Чтобы выбрать сортировку по возрастанию, нажмите клавишу 1. Для сортировки по убыванию нажмите клавишу 2");
+                    PressedKey = ReadKey();
+                    WriteLine();
+                    if ((PressedKey.Key != ConsoleKey.D1) && (PressedKey.Key != ConsoleKey.D2))
+                    {
+                        throw myException;
+                    }
+                    // вызываем метод, который в свою очередь запускает событие.
+                    OnChoiseDone(ref PressedKey);
+                }
+                catch (MyException)
+                {
+                    WriteLine();
+                    WriteLine(myException.Message);
+                    WriteLine("Нажмите любую кнопку.");
+                    ReadKey();
+                    Clear();
+                }
+                
+            }
+
+
+        }
+        protected virtual void OnChoiseDone(ref ConsoleKeyInfo PressedKey)
+        {
+            // запускаем событие
+            ChoiseDoneEvent.Invoke(PressedKey);
+        }
+        
+    }
+    // собственный тип исключения
+    class MyException : Exception
+    {
+        static string message = "Добрый вечер, не та клавиша.";
+        public MyException() : base(message)
+        {
+
+        }
+    }
+}
